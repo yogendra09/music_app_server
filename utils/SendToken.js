5519835f04b6b862bf65e2cfd474860c1ccde49d
yogendra09 <yogendrayadav168@gmail.com>
2024-03-21 15:00:31 +0530

    expires err

--- utils/SendToken.js
+++ utils/SendToken.js
@@ -2,7 +2,7 @@ exports.sendtoken = (user, statuscode, res) => {
   const token = user.getjwttoken();
 
   const options = {
-    exipres: new Date(
+    exipresIn: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     secure: true,
