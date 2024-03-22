cbe49a137ca67b4abd3edf225a5c6d0075d80879
yogendra09 <yogendrayadav168@gmail.com>
2024-03-22 14:39:34 +0530

    show token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,7 @@ exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
 
   if (!token) {
-    return next(new ErrorHandler("please login to access the resource", 401));
+    return next(new ErrorHandler("please login to access the resource"+token, 401));
   }
 
   const { id } = jwt.verify(token, process.env.JWT_SECRET);
